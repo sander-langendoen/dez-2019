fbe0481cf78a63a86291e7bee716522489f040ec
Sander  Langendoen <s.langendoen@simgroep.nl>
2019-01-01 15:39:13 +0100

    Added site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'Diëtist Eline Zuiderwijk - diëtist Voorburg, Leidschendam, Dordrecht',
+    description: 'Ik ben diëtist Eline Zuiderwijk, werkzaam in de regio Leidschendam-Voorburg en Dordrecht. Wil je afvallen met mij als jouw diëtist?',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
