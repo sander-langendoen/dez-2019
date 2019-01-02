3b2b948aa65661b99cf3d7c8bc23eda8b58a9389
Sander  Langendoen <s.langendoen@simgroep.nl>
2019-01-02 22:16:01 +0100

    Added Styled-components

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,29 @@ import React from 'react'
 
 import Navbar from '../components/Navbar'
 import Logo from '../components/Logo'
+import styled from 'styled-components'
 
 const Header = class extends React.Component {
 
 	render() {
+
+		const Container = styled.container`
+		  	max-width: 960px;
+			margin: 0 auto;
+			display: flex;
+		`;
+
 		return (
 
 			<header>
-				<div className="container">
+
+				<Container>
+			
 					<Logo />
 					<Navbar/>
-				</div>
+			
+				</Container>
+			
 			</header>
 		)
 	}
