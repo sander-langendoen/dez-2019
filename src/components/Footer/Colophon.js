6789c92f59affeee0638ed9f73e3e3cb086fda5c
Sander  Langendoen <s.langendoen@simgroep.nl>
2019-01-07 19:14:41 +0100

    added date to Colophon

--- src/components/Footer/Colophon.js
+++ src/components/Footer/Colophon.js
@@ -3,12 +3,23 @@ import React from 'react'
 
 const Colophon = class extends React.Component {
  
+	constructor() {
+        super();
+
+        var today = new Date(),
+            date = today.getFullYear();
+
+        this.state = {
+            date: date
+        };
+    }
+
  	render() {
         return (
 
 	        <div className="colophon">
-				<span>&#169; /*  echo date("Y"); Eline Zuiderwijk | */ </span>
-				<a href="http://kleefaan.nl/" rel="WordPress designer en websitebouwer" title="WordPress websitebouwer">Kleefaan.nl</a> 
+				<span id="colophonDate">&#169; {this.state.date} Eline Zuiderwijk | */ </span>
+				<span>door Sander Langendoen</span> 
 			</div>
 
 		)
